381549a89e86f7dc3ed86a347e79d7a790ddfabc
johnnygomes25 <107089229+johnnygomes25@users.noreply.github.com>
2022-10-08 23:28:06 -0300

    Projeto mongoDB

---
+++ src/main/java/com/johnnygomes/br/WorkshopgmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.johnnygomes.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopgmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopgmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnnygomes/br/WorkshopgmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnnygomes.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopgmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
